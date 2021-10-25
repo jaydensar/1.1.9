9aa3e5909cbe2077070d14657e705f62e0614414
Jayden <jayden@jaydensar.net>
2021-10-25 14:08:31 -0400

    add undo functionality

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import turtle
 turtle.color('blue')
 turtle.speed(0)
 turtle.shape('circle')
+turtle.penup()
 
 value=False
 move=False
@@ -43,9 +44,13 @@ def scroll(mouse):
     else:
         turtle.pensize(turtle.pensize()+0.5)
 
+def undo(_mouse):
+    turtle.undo()
+
 turtle.getcanvas().winfo_toplevel().bind('<ButtonPress-1>', mouse_down)
 turtle.getcanvas().winfo_toplevel().bind('<ButtonRelease-1>', mouse_up)
 turtle.getcanvas().winfo_toplevel().bind('<Motion>', motion)
 turtle.getcanvas().winfo_toplevel().bind('<MouseWheel>', scroll)
+turtle.getcanvas().winfo_toplevel().bind('<Control-z>', undo)
 
 turtle.mainloop()
