5072fa4982e55106e5ba4335b8b4e9ae968cc04c
Jayden <jayden@jaydensar.net>
2021-10-25 14:13:48 -0400

    manual updates

--- main.py
+++ main.py
@@ -1,56 +1,54 @@
 import turtle
 turtle.color('blue')
-turtle.speed(0)
 turtle.shape('circle')
 turtle.penup()
+turtle.tracer(0)
 
-value=False
-move=False
-count=0
+mouse_down = False
+count = 0
+root = turtle.getcanvas().winfo_toplevel()
 
 def goto(x,y):
-    global move
     global count
-    if move:
-        print("moving to", x, y)
-        turtle.goto(x,y)
-        count+=1
-        print(count)
-        move=False
-    else:
-        move=True
-        print("not moving")
+    print("moving to", x, y)
+    turtle.goto(x,y)
+    turtle.update()
+    count+=1
+    print(count)
 
-def mouse_down(mouse):
-    global value
-    value=True
+def mouse_down_action(mouse):
+    global mouse_down
+    mouse_down=True
     x=mouse.x-turtle.window_width()/2
     y=mouse.y-turtle.window_height()/2
     turtle.penup()
     goto(x, -y)
     turtle.pendown()
-def mouse_up(mouse):
-    global value
-    value=False
-def motion(mouse):
-    if value:
+
+def mouse_up_action(_mouse):
+    global mouse_down
+    mouse_down=False
+
+def motion_action(mouse):
+    if mouse_down:
         x=mouse.x-turtle.window_width()/2
         y=mouse.y-turtle.window_height()/2
-        # print(x,y)
         goto(x, -y)
-def scroll(mouse):
+
+def scroll_action(mouse):
     if mouse.delta<0:
         turtle.pensize(turtle.pensize()-0.5)
     else:
         turtle.pensize(turtle.pensize()+0.5)
 
-def undo(_mouse):
+def undo_action(_mouse):
     turtle.undo()
+    turtle.update()
 
-turtle.getcanvas().winfo_toplevel().bind('<ButtonPress-1>', mouse_down)
-turtle.getcanvas().winfo_toplevel().bind('<ButtonRelease-1>', mouse_up)
-turtle.getcanvas().winfo_toplevel().bind('<Motion>', motion)
-turtle.getcanvas().winfo_toplevel().bind('<MouseWheel>', scroll)
-turtle.getcanvas().winfo_toplevel().bind('<Control-z>', undo)
+root.bind('<ButtonPress-1>', mouse_down_action)
+root.bind('<ButtonRelease-1>', mouse_up_action)
+root.bind('<Motion>', motion_action)
+root.bind('<MouseWheel>', scroll_action)
+root.bind('<Control-z>', undo_action)
 
 turtle.mainloop()
